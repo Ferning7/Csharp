271b39bbf114c7bb26fbfd6cff52c96afd0f14b5
Matheus <81193702+Ferning7@users.noreply.github.com>
2025-01-29 10:20:05 -0300

    Update Program.cs

--- at04/at04/Program.cs
+++ at04/at04/Program.cs
@@ -10,6 +10,7 @@ namespace at04
 			Console.Write("Digite sim ou não: ");
 			string escolha = Console.ReadLine();
 			
+			//pode ser usado método .ToLower() para definir a resposta para letra minúscula
 			bool check = (escolha == "sim" || escolha == "Sim" || escolha == "SIM") ? true : false;
 			
 			Console.WriteLine(check);
